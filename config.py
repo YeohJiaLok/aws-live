cf30a423b284c1eaec475350a34904939d49c196
YeohJiaLok <101831332+YeohJiaLok@users.noreply.github.com>
2022-03-18 04:02:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.c8icpcctlhex.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "bait3273"
+custompass = "Bait3273"
 customdb = "employee"
 custombucket = "yeohjialok-employee"
 customregion = "us-east-1"
